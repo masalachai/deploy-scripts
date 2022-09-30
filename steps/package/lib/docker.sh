57498e747961f346a404c42b50fc51000dc74a26
Ritesh Chitlangi <ritesh@ayravat.com>
2022-09-30 16:12:45 +0800

    hack for missing env file when needed at build stage since the same docker-compose is being used for build and container start. to be refacotred later

--- steps/package/lib/docker.sh
+++ steps/package/lib/docker.sh
@@ -8,6 +8,13 @@ ds_package() {
 	copy_docker_files "$PROJECT_DEPLOY_DIR" $PROJECT_ENVIRONMENT "$1"
 
 	TAG=$(grep 'image:' docker-compose.yml | wc -l)
+	ENV_FILE_PRESENT="true"
+	COMPOSE_ENV_FILE=$(awk '/env_file:/,/\n/' docker-compose.yml | awk '{printf $2}')
+
+	if [ ! -f "$COMPOSE_ENV_FILE" ]; then
+		ENV_FILE_PRESENT="false"
+		touch "$COMPOSE_ENV_FILE"
+	fi
 
 	if [ $TAG -eq 0 ]; then
 		TIMESTAMP=$(date +%Y%m%d%H%M%S)
@@ -23,6 +30,10 @@ ds_package() {
 	fi
 
 	docker-compose build $DOCKER_COMPOSE_OPTS
+
+	if [ "$ENV_FILE_PRESENT" = "false" ]; then
+		rm -rf "$COMPOSE_ENV_FILE"
+	fi
 
 	cleanup_docker_files
 }
